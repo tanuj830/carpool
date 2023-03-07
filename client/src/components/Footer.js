284782530a615f080ad7db75c9d0bc12feec31e9
tanuj <tanujbhatt644@gmail.com>
2023-03-07 18:22:36 +0530

    UI updated: V2

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -5,21 +5,20 @@ import {FaCarCrash} from 'react-icons/fa'
 const Footer = () => {
   return (
     <div className=' text-center bg-black text-white'>
-     <hr /> <div className='container mt-2 py-5 px-5 d-flex justify-content-between'>
+     <hr /> <div className='container mt-2 py-5 px-5 flex justify-between'>
         <div className='flex items-center'>
          <div> 
-          <Link to="/" className=' text-decoration-none  fs-3'><FaCarCrash /> Carpool</Link></div>
+          <Link to="/" className=' flex items-center '><FaCarCrash /> <logo className="ml-1">Carpool</logo></Link></div>
         </div>
         <div>
-        <h3 className='lead fs-6 text-muted'>About</h3>
-          <Link to="/aboutus" className='text-decoration-none' >About</Link><br />
-          <Link to="/https://en.wikipedia.org/wiki/Carpool" className='text-decoration-none' >What is Carpool ?</Link>
+          <Link to="/aboutus" className='text-decoration-none underline' >About</Link><br />
+          <Link to="/https://en.wikipedia.org/wiki/Carpool" className='text-decoration-none underline' >What is Carpool ?</Link>
         </div>
         <div className=''>
           <div>
             <h3 className='lead fs-6 text-muted'>Social Link</h3>
           </div>
-          <a href="https://github.com/tanuj830/carpool" className='text-dark hover:bg-danger'><BsGithub /></a>
+          <a href="https://github.com/tanuj830/carpool" className='flex items-center underline'><BsGithub /><p className='ml-1'>GitHub</p></a>
         </div>
       </div>
       <small className='py-2'>© 2022 Carpool</small>
